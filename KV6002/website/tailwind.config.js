76df237571b656274d7532769e2640075ea9a107
PetrosTam <133213483+PetrosTam@users.noreply.github.com>
2024-03-26 07:35:47 +0000

    Create tailwind.config.js

---
+++ KV6002/website/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+/**
+* @author Petros Tamboutsiaris W21004471
+*/
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  /**
+  *Enable dark mode by class
+  */
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
