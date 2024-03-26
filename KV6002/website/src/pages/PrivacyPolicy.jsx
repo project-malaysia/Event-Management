c8ea35f19e642b6b2a821f28b6d766c1a46aa5bb
PetrosTam <133213483+PetrosTam@users.noreply.github.com>
2024-03-26 08:12:33 +0000

    Create PrivacyPolicy.jsx

---
+++ KV6002/website/src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PrivacyPolicyText from '../components/PrivacyPolicyText';
+import Footer from '../components/Footer';
+
+/**
+ * PrivacyPolicy page.
+ * 
+ * It represents the Privacy Policy page of the website.
+ * 
+ * @author Petros Tamboutsiaris W21004471
+ */
+
+function PrivacyPolicy() {
+    return (
+        <div className="bg-purple-200 dark:bg-gray-800 flex flex-col min-h-screen">
+            <div className="flex-grow">
+                <PrivacyPolicyText />
+            </div>
+            <footer className="mt-8">
+                <Footer />
+            </footer>
+        </div>
+    );
+}
+
+export default PrivacyPolicy;
