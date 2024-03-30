e2ff2d21a491da15eafa81b76f1e81576b638f21
Petros Tamboutsiaris <133213483+PetrosTam@users.noreply.github.com>
2024-03-30 00:05:50 +0000

    Update createNewThread.jsx

--- KV6002/website/src/components/createNewThread.jsx
+++ KV6002/website/src/components/createNewThread.jsx
@@ -26,8 +26,8 @@ function NewThreadForm({ user_id, onSubmit }) {
     return (
         <div>
             <form onSubmit={handleSubmit} className="border border-gray-300 p-4 rounded-md mb-4">
-                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Thread Title" required className="border border-gray-300 rounded-md p-2 w-full" />
-                <textarea value={content} onChange={(e) => setContent(e.target.value)} placeholder="Thread Content" required className="border border-gray-300 rounded-md p-2 w-full" />
+                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Thread Title" required className="border border-gray-300 rounded-md p-2 w-full dark:text-gray-900" />
+                <textarea value={content} onChange={(e) => setContent(e.target.value)} placeholder="Thread Content" required className="border border-gray-300 rounded-md p-2 w-full dark:text-gray-900" />
                 <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add Thread</button>
             </form>
         </div>
