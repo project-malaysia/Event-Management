584b61faa79941ceeb313350897dd2f36ae2d8b9
Petros Tamboutsiaris <133213483+PetrosTam@users.noreply.github.com>
2024-03-31 00:31:17 +0000

    Update settings.php

--- KV6002/api/petros/config/settings.php
+++ KV6002/api/petros/config/settings.php
@@ -1,1 +1,12 @@
+<?php
 
+/**
+ * settings.php
+ * 
+ * In settings.php we use define() functions to set global constants.
+ * 
+ * @author Petros Tamboutsiaris W21004471
+ */
+
+define('BASEPATH','/kv6002/api');
+define('DATABASE', 'db/pt.sqlite');
